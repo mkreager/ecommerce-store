ac42c027d40ef84ac4e70bfec77b12925a09a882
Mike Kreager <mkreager@users.noreply.github.com>
2017-12-09 14:22:55 -0500

    Update AuthenticationFilter.java

--- src/com/store/filters/AuthenticationFilter.java
+++ src/com/store/filters/AuthenticationFilter.java
@@ -25,30 +25,28 @@ import javax.servlet.http.HttpSession;
 		"com.store.controllers.PaymentControllerServlet" })
 public class AuthenticationFilter implements Filter {
 
-	FilterConfig filterConfig;
+    FilterConfig filterConfig;
 	
-	public void init(FilterConfig fConfig) throws ServletException {
-		this.filterConfig = fConfig;
-	}
+    public void init(FilterConfig fConfig) throws ServletException {
+	this.filterConfig = fConfig;
+    }
 	
-	public void destroy() {
-		
-	}
+    public void destroy() {	
+    }
 
-	
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {    
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {    
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         
         // Get the session
         HttpSession session = req.getSession();
         if (session.getAttribute("username") == null || session.getAttribute("password") == null) {
-        	// Set a flag to indicate a checkout request so we can direct the user back to their order after login
-        	session.setAttribute("checkout", "true");
-        	session.setAttribute("message", "You need to login first.");
+            // Set a flag to indicate a checkout request so we can direct the user back to their order after login
+            session.setAttribute("checkout", "true");
+            session.setAttribute("message", "You need to login first.");
             res.sendRedirect(req.getContextPath() + "/login");
         } else {
             chain.doFilter(request, response);
         }
-	}
+    }
 }
